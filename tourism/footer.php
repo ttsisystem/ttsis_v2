02eb82524f6f84a519318e099f00897e897221f3
ttsisystem <97573563+ttsisystem@users.noreply.github.com>
2022-01-12 10:54:31 +0800

    Add files via upload

---
+++ tourism/footer.php
@@ -0,0 +1,92 @@
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <footer class="bg-dark" id="tempaltemo_footer">
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-primary border-bottom pb-3 border-light logo">Tumauini Officials</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li>
+                            <i class="fas fa-map-marker-alt fa-fw text-light"></i>
+                            San Pedro, Tumauini, Isabela
+                        </li>
+                        <li>
+                            <i class="fa fa-phone fa-fw"></i>
+                            <a class="text-decoration-none text-light" href="tel:010-020-0340">+6392-6963-7570</a>
+                        </li>
+                        <li>
+                            <i class="fa fa-envelope fa-fw "></i>
+                            <a class="text-decoration-none text-light" href="mailto:yumauini@gmail.com">tumauini@gmail.com</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Useful Links</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li><a class="text-decoration-none text-light" href="https://www.facebook.com/profile.php?id=100068903302758" target="_blank">PNP at Kumunidad</a></li>
+                        <li><a class="text-decoration-none text-light" href="https://www.facebook.com/TumauiniMHO" target="_blank">Tumauini MHO</a></li>
+                        <li><a class="text-decoration-none text-light" href="https://www.facebook.com/BayanNgTUmauiniOfficial" target="_blank">Bayan ng Tumauini</a></li>
+                        <li><a class="text-decoration-none text-light" href="https://www.facebook.com/tumauinips.valleycops/" target="_blank">Tumauini PCR</a></li>
+                    </ul>
+                </div>
+
+                <div class="col-md-4 pt-5">
+                    <h2 class="h2 text-light border-bottom pb-3 border-light">Further Info</h2>
+                    <ul class="list-unstyled text-light footer-link-list">
+                        <li><a class="text-decoration-none text-light" href="h.php">Home</a></li>
+                        <li><a class="text-decoration-none text-light" href="province.php">About Us</a></li>
+                        <li><a class="text-decoration-none text-light" href="news.php">News and Events</a></li>
+                        <li><a class="text-decoration-none text-light" href="contact.php">Contact</a></li>
+                    </ul>
+                </div>
+
+            </div>
+
+            <div class="row text-light mb-4">
+                <div class="col-12 mb-3">
+                    <div class="w-100 my-3 border-top border-light"></div>
+                </div>
+                <div class="col-auto me-auto">
+                    <ul class="list-inline text-left footer-icons">
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="http://facebook.com/"><i class="fab fa-facebook-f fa-lg fa-fw"></i></a>
+                        </li>
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="https://www.instagram.com/"><i class="fab fa-instagram fa-lg fa-fw"></i></a>
+                        </li>
+                        <li class="list-inline-item border border-light rounded-circle text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="#"><i class="fab fa-twitter fa-lg fa-fw"></i></a>
+                        </li>
+                        <li class="list-inline-item border-light text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="#"><i class=""><img style="width: 75px"  src="logo/12.jpg"></i></a>
+                        </li>
+                        <li class="list-inline-item border-light text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="#"><i class=""><img style="width: 75px"  src="logo/13.jpg"></i></a>
+                        </li>
+                        <li class="list-inline-item border-light text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="#"><i class=""><img style="width: 75px"  src="logo/14.jpg"></i></a>
+                        </li>
+                        <li class="list-inline-item  border-light text-center">
+                            <a class="text-light text-decoration-none" target="_blank" href="#"><i class=""><img style="width: 75px" src="logo/15.jpg"></i></a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="w-100 bg-black py-3">
+            <div class="container">
+                <div class="row pt-2">
+                    <div class="col-12">
+                        <p class="text-left text-light">
+                            Copyright &copy; 2021 Tumauini Tourist Information System 
+                            | Designed by <a rel="sponsored" href="" class="text-light">ISU Cabagan - BSiT</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer>
